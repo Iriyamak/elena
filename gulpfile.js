a3e336a7b349d77df89cd8176e149eb1712bf915
Iryna Moiseieva <iriyamois@gmail.com>
2018-02-18 21:29:59 +0200

    full markup

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,35 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
-const gulpIf = require('gulp-if');
-const debug = require('gulp-debug');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const gulpIf = require("gulp-if");
+const debug = require("gulp-debug");
+const browserSync = require("browser-sync").create();
 
 const isDev = true;
 
-gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
+gulp.task("sass", function() {
+  return gulp
+    .src("./scss/**/*.scss")
     .pipe(gulpIf(isDev, sourcemaps.init()))
-    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(autoprefixer())
     .pipe(sass())
-    .pipe(concat('./style.min.css'))
-    .pipe(cleanCss())
+    .pipe(concat("./style.min.css"))
     .pipe(gulpIf(isDev, sourcemaps.write()))
-	.pipe(gulp.dest('dist'))
+    .pipe(gulp.dest("dist"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('serv', function () {
+gulp.task("serv", function() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: "./"
     },
     notify: false
   });
-  gulp.watch(['./scss/*.scss', './scss/*/*.scss'], ['sass']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch(["./scss/*.scss", "./scss/*/*.scss"], ["sass"]);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'serv']);
+gulp.task("default", ["sass", "serv"]);
